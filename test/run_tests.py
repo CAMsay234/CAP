6070819eb719721c338b9c3338aa2b02e5ab7d34
Alejandro <luis.londono@upb.edu.co>
2024-10-10 11:30:40 -0500

    Archivo para pruebas generales

---
+++ test/run_tests.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import unittest
+
+# Añadir el directorio raíz de CAP
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Imprimir sys.path para verificar que contiene la ruta correcta
+print(sys.path)
+
+# Importa las pruebas
+from test_evaluaciones import TestEvaluacionesNeuropsicologicas
+
+# Ejecuta las pruebas
+if __name__ == '__main__':
+    unittest.main()
