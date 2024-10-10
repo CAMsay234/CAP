c714ac5ead2b27eb38c13ee3546bfc911db5ecd0
Alejandro <luis.londono@upb.edu.co>
2024-10-10 16:49:27 -0500

    Corrección pruebas evaluaciones

--- test/run_tests.py
+++ test/run_tests.py
@@ -6,7 +6,7 @@ import unittest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Imprimir sys.path para verificar que contiene la ruta correcta
-print(sys.path)
+#print(sys.path)
 
 # Importa las pruebas
 from test_evaluaciones import TestEvaluacionesNeuropsicologicas
--- test/test_evaluaciones.py
+++ test/test_evaluaciones.py
@@ -1,14 +1,21 @@
 import unittest
-from app import app, db
+import json
+from app import create_app, db
 from app.models.evaluacion_neuropsicologica import EvaluacionNeuropsicologica
+
+app = create_app()
 
 class TestEvaluacionesNeuropsicologicas(unittest.TestCase):
 
     def setUp(self):
-        # Configurar el cliente de pruebas de Flask y la base de datos
-        self.app = app.test_client()
-        self.app.testing = True
+        # Crea y activa un contexto de aplicación
+        self.app_context = app.app_context()
+        self.app_context.push()
 
+        # Configurar el cliente de pruebas de Flask con el contexto de aplicación activo
+        self.client = app.test_client()
+        self.client.testing = True
+        
         # Crear tablas en la base de datos (si no existen)
         db.create_all()
 
@@ -30,9 +37,11 @@ class TestEvaluacionesNeuropsicologicas(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    # Prueba para la creación de una evaluación (POST)
+        # Desactivar el contexto de la aplicación
+        self.app_context.pop()
+
     def test_crear_evaluacion(self):
-        response = self.app.post('/evaluaciones', json={
+        response = self.client.post('/evaluaciones', json={
             'codigo_hc': 2,
             'id_prueba': 2,
             'id_subprueba': 2,
@@ -41,37 +50,40 @@ class TestEvaluacionesNeuropsicologicas(unittest.TestCase):
             'desviacion_estandar': 15,
             'interpretacion': 'Levemente bajo'
         })
+        data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 201)
-        self.assertIn("Evaluación neuropsicológica creada exitosamente", response.get_data(as_text=True))
+        self.assertIn("Evaluación neuropsicológica creada exitosamente", data['message'])
 
-    # Prueba para obtener todas las evaluaciones (GET)
     def test_obtener_evaluaciones(self):
-        response = self.app.get('/evaluaciones')
+        response = self.client.get('/evaluaciones')
+        data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Normal", response.data)
+        # Verifica que la respuesta contiene la palabra "Normal" en algún punto
+        self.assertTrue(any("Normal" in evaluacion['interpretacion'] for evaluacion in data if isinstance(evaluacion, dict)))
 
-    # Prueba para obtener una evaluación por clave primaria (GET)
     def test_obtener_evaluacion(self):
-        response = self.app.get('/evaluaciones/1/1/1')
+        response = self.client.get('/evaluaciones/1/1/1')
+        data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Normal", response.data)
+        # Verifica que la respuesta contiene "Normal" como parte de la interpretación
+        self.assertIn("Normal", data['interpretacion'])
 
-    # Prueba para actualizar una evaluación (PUT)
     def test_actualizar_evaluacion(self):
-        response = self.app.put('/evaluaciones/1/1/1', json={
+        response = self.client.put('/evaluaciones/1/1/1', json={
             'puntaje': 95,
             'media': 100,
             'desviacion_estandar': 15,
             'interpretacion': 'Levemente alto'
         })
+        data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Evaluación neuropsicológica actualizada exitosamente", response.get_data(as_text=True))
+        self.assertIn("Evaluación neuropsicológica actualizada exitosamente", data['message'])
 
-    # Prueba para eliminar una evaluación (DELETE)
     def test_eliminar_evaluacion(self):
-        response = self.app.delete('/evaluaciones/1/1/1')
+        response = self.client.delete('/evaluaciones/1/1/1')
+        data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Evaluación neuropsicológica eliminada exitosamente", response.get_data(as_text=True))
+        self.assertIn("Evaluación neuropsicológica eliminada exitosamente", data['message'])
 
 if __name__ == '__main__':
     unittest.main()
