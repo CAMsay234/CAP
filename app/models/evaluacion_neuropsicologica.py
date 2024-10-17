57b4ec291b98ee48d74bdcc1b0faae44f95a671a
Camilo V <77682959+CAMsay234@users.noreply.github.com>
2024-10-17 15:42:45 -0500

    update atencion y concentracion

--- app/models/evaluacion_neuropsicologica.py
+++ app/models/evaluacion_neuropsicologica.py
@@ -3,15 +3,15 @@ from app import db
 
 class EvaluacionNeuropsicologica(db.Model):
     __tablename__ = 'evaluaciones_neuropsicologicas'  # Nombre de la tabla en la base de datos
-
     # Clave primaria compuesta
     codigo_hc = db.Column(db.Integer, db.ForeignKey('historias_clinicas.codigo_hc'), primary_key=True, nullable=False)
     id_prueba = db.Column(db.Integer, db.ForeignKey('pruebas.id'), primary_key=True, nullable=False)
     id_subprueba = db.Column(db.Integer, db.ForeignKey('sub_pruebas.id'), primary_key=True, nullable=False)
-    puntaje = db.Column(db.Float, nullable=False)
-    media = db.Column(db.Float, nullable=False)
-    desviacion_estandar = db.Column(db.Float, nullable=False)
-    interpretacion = db.Column(db.Text, nullable=False)
+    puntaje = db.Column(db.String, nullable=False, default="N/A")  # Usar valor por defecto
+    media = db.Column(db.String, nullable=False, default="N/A")  # Usar valor por defecto
+    desviacion_estandar = db.Column(db.String, nullable=False, default="N/A")  # Usar valor por defecto
+    escalar = db.Column(db.String, nullable=False, default="N/A")  # Usar valor por defecto
+    interpretacion = db.Column(db.Text, nullable=False, default="N/A")  # Usar valor por defecto
 
 
     def __repr__(self):
--- app/models/sub_prueba.py
+++ app/models/sub_prueba.py
@@ -4,7 +4,7 @@ class SubPrueba(db.Model):
     __tablename__ = 'sub_pruebas'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_prueba = db.Column(db.Integer, db.ForeignKey('pruebas.id'), nullable=False)
-    nombre = db.Column(db.String(100), nullable=False, unique=True)
+    nombre = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f'<SubPrueba {self.nombre}>'
